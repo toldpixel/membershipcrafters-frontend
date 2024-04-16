b083ea5358792f1e4b9a27c3936621872a41cb68
xorpad <software.nutshell@gmail.com>
2024-04-16 18:18:50 +0200

    remove nuxt-icons bug Property 'nuxtIcon' is missing in type 'AppConfig' when update to 2.11.0 #1045 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/color-mode',
     '@nuxtjs/supabase',
+    'nuxt-icon',
     (_options, nuxt) => {
       nuxt.hooks.hook('vite:extendConfig', (config) => {
         // @ts-expect-error
