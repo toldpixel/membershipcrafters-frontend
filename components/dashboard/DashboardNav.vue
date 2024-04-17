bb16094aff008f3d2897526c683d506bf0b9b20c
xorpad <software.nutshell@gmail.com>
2024-04-17 12:31:56 +0200

    profile menu

--- components/dashboard/DashboardNav.vue
+++ components/dashboard/DashboardNav.vue
@@ -1,58 +1,81 @@
 <script setup lang="ts">
-
+const items = [
+    'mail',
+    'Profile',
+    'Settings',
+    'Help Center',
+    'Create a Community',
+    'Discover Communities',
+    'Log out'
+]
 const searchQuery = ref('')
 
 function search() {
 
 }
+
+function profileOption(index: number) {
+    console.log(index)
+}
 </script>
 
 <template>
     <div class="flex p-1 items-center space-x-7 bg-white">
-            <div>Logo</div>
-            <div class="p-2 rounded-full hover:bg-slate-200 cursor-pointer">
-                <Icon size="24" name="material-symbols:keyboard-double-arrow-down-rounded"></Icon>
+        <div>Logo</div>
+        <div class="p-2 rounded-full hover:bg-slate-200 cursor-pointer">
+            <Icon size="24" name="material-symbols:keyboard-double-arrow-down-rounded"></Icon>
+            <client-only>
                 <v-tooltip text="Tooltip" location="bottom" activator="parent">
                     Switch communities
                 </v-tooltip>
-            </div>
+            </client-only>
+        </div>
 
-            <div class="flex justify-center">
-                <div class="flex border-2 border-gray-200 rounded">
-                    <input type="text" class="px-4 py-2 w-80" placeholder="Suche..." v-model="searchQuery" />
-                    <button class="flex items-center justify-center px-4 border-l" @click="search">
-                        Suchen
-                    </button>
-                </div>
+        <div class="flex justify-center">
+            <div class="flex border-2 border-gray-200 rounded">
+                <input type="text" class="px-4 py-2 w-80" placeholder="Suche..." v-model="searchQuery" />
+                <button class="flex items-center justify-center px-4 border-l" @click="search">
+                    Suchen
+                </button>
             </div>
+        </div>
 
-            <div class="p-2 rounded-full hover:bg-slate-200 cursor-pointer">
-                <Icon size="24" name="material-symbols:chat-bubble-outline-rounded"></Icon>
+        <div class="p-2 rounded-full hover:bg-slate-200 cursor-pointer">
+            <Icon size="24" name="material-symbols:chat-bubble-outline-rounded"></Icon>
+            <client-only>
                 <v-tooltip text="Tooltip" location="bottom" activator="parent">
                     Chats
                 </v-tooltip>
+            </client-only>
+          
 
-            </div>
+        </div>
 
 
-            <div class="p-2 rounded-full hover:bg-slate-200 cursor-pointer">
-                <Icon size="24" name="material-symbols:doorbell-outline-sharp"></Icon>
+        <div class="p-2 rounded-full hover:bg-slate-200 cursor-pointer">
+            <Icon size="24" name="material-symbols:doorbell-outline-sharp"></Icon>
+            <client-only>
                 <v-tooltip text="Tooltip" location="bottom" activator="parent">
                     Notifications
                 </v-tooltip>
-            </div>
-
-
-            <div>
-                <!--
-                  <Avatar size="sm">
-                    <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-            -->
-
-            <v-avatar color="surface-variant"></v-avatar>
-
-            </div>
+            </client-only>
+            
         </div>
+        <div>
+            <v-menu transition="scale-transition">
+                <template v-slot:activator="{ props }">
+                    <v-avatar v-bind="props" color="surface-variant"></v-avatar>
+                </template>
+                <v-list density="compact" class="my-2">
+                    <v-list-item @click="profileOption(index)" v-for="(item, index) in items" :key="index"
+                        :value="index">
+                        <v-list-item-title>
+                            {{ item }}
+                        </v-list-item-title>
+                    </v-list-item>
+                    <hr>
+                </v-list>
+            </v-menu>
+        </div>
+    </div>
 </template>
